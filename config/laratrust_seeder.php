c861680dbd3bfccd3a09852e66c0a8c28e9a52a8
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-04-09 20:49:44 +0200

    Installing a laratrust package

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'create_users' => false,
+
+    'truncate_tables' => true,
+
+    'roles_structure' => [
+        'superadministrator' => [
+            'users' => 'c,r,u,d',
+            'payments' => 'c,r,u,d',
+            'profile' => 'r,u',
+        ],
+        'administrator' => [
+            'users' => 'c,r,u,d',
+            'profile' => 'r,u',
+        ],
+        'user' => [
+            'profile' => 'r,u',
+        ],
+        'role_name' => [
+            'module_1_name' => 'c,r,u,d',
+        ],
+    ],
+
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete',
+    ],
+];
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::view('/', 'welcome');
+//*Main route
+Volt::route('/', 'pages.auth.login')
+    ->name('login');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
