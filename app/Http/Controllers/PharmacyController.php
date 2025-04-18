f159b48a8251f476f54e67a0842a02761cc1fd95
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-04-19 01:28:21 +0200

    Index method - Pharmacy API

--- app/Http/Controllers/PharmacyController.php
+++ app/Http/Controllers/PharmacyController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pharmacy;
 use Illuminate\Http\Request;
 
 class PharmacyController extends Controller
 {
-    //
+    public function index()
+    {
+        $pharmacies = Pharmacy::all();
+
+        return response()->json([
+            'status' => true,
+            'pharmacies' => $pharmacies
+        ]);
+    }
 }
