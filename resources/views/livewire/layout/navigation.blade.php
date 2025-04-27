6a93aa86573b37bb9426e21e685e9c6af6179115
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-04-27 21:11:24 +0200

    Doing some updates in the UX

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -72,6 +72,9 @@ new class extends Component
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            
+                            <img src="./assets/dif.jpg" alt="Profile Photo" style="margin-right:0.5rem"
+                            class="w-8 h-8 rounded-full object-cover border-2 border-gray-300 dark:border-gray-600" />
                             <div x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
 
                             <div class="ms-1">
